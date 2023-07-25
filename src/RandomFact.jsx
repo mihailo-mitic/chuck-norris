54b8e56872c19760184c080c831736aaa923fb0f
mihailo-mitic <mihailomitic@gmail.com>
2023-07-25 23:30:59 +0200

    3

--- src/RandomFact.jsx
+++ src/RandomFact.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 const RandomFact = () => {
-  const [fact, setFact] = useState("random fact loading...");
+  const [fact, setFact] = useState("random fact loadin");
   useEffect(
     () =>
       async function fetchData() {
